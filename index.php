6620d079780ced8e49adc054d93449ffe3cb44c2
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2022-05-07 23:11:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,8 @@
 				<input type="text" name="act_name" id="act_name">
 				<label for="act_gender">Actor Gender:</label>
 				<input type="text" name="act_gender" id="act_gender">
-			</p>
-
-
-			
-<h1>Director</h1>
-			
+			</p>			
+<h1>Director</h1>		
 <p>
 				<label for="dir_id">Director ID:</label>
 				<input type="text" name="dir_id" id="dir_id">
@@ -32,10 +28,7 @@
 				<label for="dir_phone">Director Phone:</label>
 				<input type="text" name="dir_phone" id="dir_phone">
 			</p>
-
 	<h1>Movie</h1>
-
-
 <p>
 				<label for="movie_id">Movie ID:</label>
 				<input type="text" name="movie_id" id="movie_id">
@@ -46,24 +39,18 @@
 				<label for="movie_lang">Language:</label>
 				<input type="text" name="movie_lang" id="movie_lang">
 			</p>
-
 	<h1>Movie Cast</h1>
-
 	<p>
 				<label for="role">Role:</label>
 				<input type="text" name="role" id="role">
 			</p>
-
 		<h1>Rating</h1>
-			
 	<p>
 				<label for="rat">Rating:</label>
 				<input type="text" name="rat" id="rat">
 			</p>
-
 			<input type="submit" value="Submit">
 		</form>
 	</center>
 </body>
-
 </html>
